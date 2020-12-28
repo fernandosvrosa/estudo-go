d262a449900392da46e2689962269d50cb7918ce
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2020-12-28 18:57:51 -0300

    server cap1

---
+++ lpgo/ch1/server1.go
@@ -0,0 +1,18 @@
+// server1 é uma servidor "eco" minimo
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler ecoa o componente Path do URL requisitado
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
